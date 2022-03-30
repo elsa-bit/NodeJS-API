1b951fa2c6fb349125f8046a282a17be36ad906c
Elsa <elsafirmin@hotmail.fr>
2022-03-30 15:03:48 +0200

    Change file

--- index.mjs
+++ index.mjs
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import morgan from "morgan";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 //app.use(morgan('tiny'));
