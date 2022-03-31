a63812d6fa996915969b8f09506d78b6666dd317
Elsa <elsafirmin@hotmail.fr>
2022-03-31 11:56:30 +0200

    Function addProduct & getProductsAsync

--- client/index.mjs
+++ client/index.mjs
@@ -3,14 +3,38 @@ import axios from "axios";
 //GET PRODUCT
 const getProducts = () => {
     const url = 'http://localhost:3000/products';
-    axios(url)
-    .then(function (response){
-        console.log(response.data);
-    })
+    axios.get(url)
+        .then(function (response) {
+            console.log(response.data);
+        })
 };
 
-getProducts();
+//ADD PRODUCT
+const addProduct = () => {
+    const url = 'http://localhost:3000/products';
+    const info = {
+        name: 'peche',
+        quantity: 2
+    }
+    axios.post(url, info)
+        .then(function (response) {
+            console.log(response.data);
+        })
+};
 
+//GET PRODUCT ASYNC
+const getProductsAsync = async () => {
+    const url = 'http://localhost:3000/products';
+    await axios.get(url)
+        .then(function (response) {
+            console.log(response.data);
+        })
+};
+
+
+getProducts();
+addProduct();
+getProductsAsync();
 
 /*
 (() => {
