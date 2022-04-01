bdfe1a669d7d72d1bc35bf9b332ee6177b54b28a
Elsa <elsafirmin@hotmail.fr>
2022-04-01 13:14:02 +0200

    Swagger

--- server/index.mjs
+++ server/index.mjs
@@ -2,6 +2,8 @@ import express from "express"
 import router from "./route.mjs";
 import bodyParser from "body-parser";
 import morgan from "morgan";
+import swaggerUI from "swagger-ui-express";
+import docs from './docs/index.mjs'
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 //app.use(morgan('tiny'));
 app.use(router)
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(docs));
 
 
 app.listen(port, () => {
